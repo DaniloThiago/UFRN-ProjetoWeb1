a805596de5088ac421bbe597422a4400800f74eb
Danilo Oliveira <danilo.t.a.oliveira@gmail.com>
2021-03-12 01:11:40 -0300

    add basic function

--- src/script.js
+++ src/script.js
@@ -1,5 +1,56 @@
-const calc = document.querySelector('#calc');
+const screen = document.querySelector('#screen');
+const operator = {
+    '+': function (x, y) {
+        return x + y
+    },
+    '-': function (x, y) {
+        return x - y
+    },
+    '*': function (x, y) {
+        return x * y
+    },
+    '/': function (x, y) {
+        return x / y
+    },
+};
 
+var numberOne, op;
 function clickButton(el) {
-    console.log(el.value)
+    
+    switch (el.value) {
+        case "equal":
+            screen.value = numberOne = operator[op](+numberOne, +screen.value);
+            break;
+        case "clear":
+            screen.value = "0";
+            numberOne = 0;
+            break;
+        case '+':
+            numberOne = screen.value;
+            screen.value = el.value;
+            op = el.value;
+            break;
+        case '-':
+            numberOne = screen.value;
+            screen.value = el.value;
+            op = el.value;
+            break;
+        case '*':
+            numberOne = screen.value;
+            screen.value = el.value;
+            op = el.value;
+            break;
+        case '/':
+            numberOne = screen.value;
+            screen.value = el.value;
+            op = el.value;
+            break;
+        default:
+            if(!!+screen.value && screen.value != '0') {
+                screen.value += el.value;
+            } else {
+                screen.value = el.value;
+            }
+            break;
+    }
 }
